de1552b48b3019037e7619fea316fb4ddbd953b3
Christopher Tero <christopher.tero@gmail.com>
2019-04-04 08:45:01 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,22 @@
+def my_collect(collection)
+  array = []
+  i = 0
+  while i < collection.length
+    array << yield(collection[i])
+    i += 1
+  end
+  array
+end
 
 
+=begin
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(students) do |student|
+  student.split(" ").first
+end
+
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(languages) do |language|
+  language.upcase
+end
+=end
