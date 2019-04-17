34f674212453bc25d63d26e653ed3468c62b2a6f
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-17 13:28:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(languages)
+  if block_given?
+  i = 0 
+  new_languages = []
+  while i < languages.length
+  
+  new_languages[i] = yield(languages[i])
+  i = i + 1
+end
+new_languages
 
+else
+  puts "Hey! No block was given!"
+end
+
+end
 
